6189b823e261287bd81416f75e4080ecdefb4278
CollinMoore16 <93558570+CollinMoore16@users.noreply.github.com>
2024-04-04 15:53:25 -0400

    Add files via upload

---
+++ Python/main.py
@@ -0,0 +1,55 @@
+##print("Hello World!")
+
+#-------------------Rectangle-------------------------
+#length = float(input("enter the length of the rectangle: "))
+#width = float(input("enter the width of the rectangle: "))
+#area = length * width 
+#print("The area of the rectangle is: ", area)
+
+#----------------Even-Or-Odd---------------------------
+#num = input ("enter a number: ")
+#if num % 2 == 0:
+#    print(num, "is even")
+#else:
+#    print(num, "is odd")
+
+#num = int(input("enter a number: "))  # Convert input string to integer
+#
+#if num % 2 == 0:
+#    print(num, "is even")
+#else:
+#    print(num, "is odd")
+
+#----------------Min-And-Max-Values----------------------------
+#num1 = int(input("enter the first number: "))
+#num2 = int(input("enter the second number: "))
+#num3 = int(input("enter the third number: "))
+
+#maxvalue = max(num1,num2,num3)
+#print("the max number is: ", maxvalue)
+
+#minvalue = min(num1,num2,num3)
+#print("the mininum number is: ", minvalue)
+
+#---------------Factorials----------------------
+#num = int(input("enter a number: "))
+#factorial = 1 
+#for i in range(1, num + 1):
+#    factorial *- i 
+#print("the factorial of", num, "is", factorial)
+
+
+#---------------Reverse-Loop-------------------
+#string = input("enter a string value:")
+#
+#reverseString = string[::-1]
+#print("Reverse String:", reverseString)
+
+#--------------Fibonacci-----------------------
+#n = int(input("provide the story point number of our task: "))
+#a, b = 0, 1
+#print("Fibonacci Sequence: ")
+#for _ in range(n): 
+#    print(a, end =" ")
+#    a, b = b, a+b 
+
